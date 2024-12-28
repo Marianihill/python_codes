f0206a199a47bd9ef8aa58f6b2fb36e62d4b17c2
Marianihill A <marianihillantonysekar@gmail.com>
2024-12-28 12:58:08 +0530

    Add files via upload

---
+++ countries migration.py
@@ -0,0 +1,160 @@
+countries_list = ['Switzerland','Japan','Unites States','Canada','Australia','Sweden','Germany','United Kingdom','New Zeland','Denmark','Norway','France','Netherland','Singapore',
+                  'Italy','China','United Arab Emirates','South Korea','Spain','Finland','Austria','Iceland','Belgium','Ireland','Qatar']
+print(countries_list)
+
+name = input("Enter your name as it is in passport: ")
+country = input("Enter you country name: ")
+
+if country in countries_list:
+  if country =='Switzerland':
+    num=int(input("Enter you phone number: +41"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='Japan':
+    num=int(input("Enter you phone number: +81"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='United States':
+    num=int(input("Enter you phone number: +1"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='Canada':
+    num=int(input("Enter you phone number: +1"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='Australia':
+    num=int(input("Enter you phone number: +61"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='Sweden':
+    num=int(input("Enter you phone number: +46"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='Germany':
+    num=int(input("Enter you phone number: +49"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='United Kingdom':
+    num=int(input("Enter you phone number: +44"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='New Zeland':
+    num=int(input("Enter you phone number: +64"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='Denmark':
+    num=int(input("Enter you phone number: +45"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='Norway':
+    num=int(input("Enter you phone number: +47"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='France':
+    num=int(input("Enter you phone number: +33"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='Netherland':
+    num=int(input("Enter you phone number: +31"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='Singapore':
+    num=int(input("Enter you phone number: +65"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='Italy':
+    num=int(input("Enter you phone number: +39"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='China':
+    num=int(input("Enter you phone number: +86"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='United Arab Emirates':
+    num=int(input("Enter you phone number: +971"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='South Korea':
+    num=int(input("Enter you phone number: +82"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='Spain':
+    num=int(input("Enter you phone number: +34"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='Finland':
+    num=int(input("Enter you phone number: +358"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='Austria':
+    num=int(input("Enter you phone number: +43"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='Iceland':
+    num=int(input("Enter you phone number: +354"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='Belgium':
+    num=int(input("Enter you phone number: +32"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='Ireland':
+    num=int(input("Enter you phone number: +353"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+  elif country =='Qatar':
+    num=int(input("Enter you phone number: +974"))
+    print("Hello",name,"!, You're from",country)
+    print("Your contact number is:", num)
+    print("And you are permitted to go to",country)
+
+else:
+     print("You are not in the mentioned country list.")
